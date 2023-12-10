aebbf86b59e31a217c24d67dcf483fe8371bebdf
Ari Stassinopoulos <stassinopoulosari@gmail.com>
2023-12-10 15:16:50 -0800

    Reorganize resource files, refactor code, add inline commentary

--- js/base.js
+++ js/base.js
@@ -1,14 +1,19 @@
 (() => {
   window.icl = {
-    logs: [],
     logLevel:
       (location.hostname != "icl.ucsd.it" || location.search.includes("log")) &&
       !location.search.includes("nolog"),
     log: (text) => {
-      icl.logs.push(text);
       if (icl.logLevel) {
         console.log(text);
       }
     },
+    defaultArray: (length, defaultValue) =>
+      Array.apply(null, { length: length }).map(() => defaultValue),
+    professorLink: (professor) =>
+      "https://act.ucsd.edu/directory/search?" +
+      "last={{last}}&first={{first}}&searchType=0"
+        .replace("{{last}}", professor.split(" ")[1])
+        .replace("{{first}}", professor.split(" ")[0]),
   };
 })();
--- js/network.js
+++ js/network.js
@@ -1,6 +1,6 @@
 (() => {
-  const retrieveClassrooms = (path) => {
-      return new Promise((resolve, reject) => {
+  const doRequest = (path) =>
+      new Promise((resolve, reject) => {
         const request = new XMLHttpRequest(),
           listener = () => {
             if (request.readyState == 4) {
@@ -15,26 +15,12 @@
         request.onreadystatechange = listener;
         request.open("GET", path, true);
         request.send();
-      });
-    },
-    retrieveGAClassroomList = (path) => {
-      return new Promise((resolve, reject) => {
-        const request = new XMLHttpRequest(),
-          listener = () => {
-            if (request.readyState == 4) {
-              if (!(request.status == 200 && request.responseText !== null)) {
-                // Handle error
-                return reject([request.status, request.responseText]);
-              }
-              // Pass classroom response text to the app
-              return resolve(JSON.parse(request.responseText));
-            }
-          };
-        request.onreadystatechange = listener;
-        request.open("GET", path, true);
-        request.send();
-      });
-    };
+      }),
+    retrieveClassrooms = () => doRequest(icl.CURRENT_PATH),
+    retrieveGAClassroomList = () =>
+      doRequest(window.icl.GA_CLASSROOMS_PATH).then((response) =>
+        JSON.parse(response),
+      );
 
   // Export
   window.icl.retrieveClassrooms = retrieveClassrooms;
